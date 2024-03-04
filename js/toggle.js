9e53d6ca06ccbf6d70df1a22e443a304f5072f50
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-04 18:19:07 -0500

    Create toggle.js and toggle.min.js
    Introduced a new event-driven toggling functionality in toggle.js and its minified version toggle.min.js. This script facilitates showing and hiding of elements based on the "value" and "toggle-hidden" attributes. The new event listener effectively monitors clicks, prevents the default behavior, and triggers custom "toggle" events with the newly updated value.

---
+++ js/toggle.js
@@ -0,0 +1,31 @@
+$(document).on("load", () => {
+    const toggles = $("toggle");
+
+
+    $(`[toggle-hidden]`).each((_, element) => {
+        let target = $(element);
+        let toggle = $(`#${target.attr("toggle-hidden")}`);
+        if (toggle.attr("value") === "true") {
+            target.show();
+        } else {
+            target.hide();
+        }
+    });
+    toggles.off("click"); // Remove any existing click event listeners.
+    // Add a click event listener to all elements with the class "toggle".
+    toggles.on("click", (e) => {
+        // Prevent the default click behavior.
+        e.preventDefault();
+        // Get the target of the click event.
+        let target = $(e.target);
+        // Get the current value of the "value" attribute of the target.
+        let value = target.attr("value") === "true";
+        // Set the "value" attribute of the target to the opposite of its current value.
+        target.attr("value", !value);
+        // Trigger a "toggle" event on the target with the new value.
+        target.trigger("toggle", [{value: !value}]);
+
+        // Toggles the visibility of the target element and the element with the id specified in the "toggle-hidden" attribute of the target.
+        $(`[toggle-hidden="${target.attr("id")}"]`).toggle();
+    });
+});
